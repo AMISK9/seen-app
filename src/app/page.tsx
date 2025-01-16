62cf034682f7eb83bf7a99fda84bdd751ace7f3b
AMISK9 <124784370+AMISK9@users.noreply.github.com>
2025-01-16 21:20:50 +0200

    added schema to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
-import {Button} from "@/components/ui/button";
-
 export default function Home() {
   return (
     <h1>home page content</h1>
